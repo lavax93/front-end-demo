7e038a66ed338a53d2ea341fac89852ac1b064b9
LAvAX <yuanxin@nbeport.com>
2017-12-06 14:40:58 +0800

    增加maptalks

---
+++ jsDemo/maptalks/assets/js/sidebar.js
@@ -0,0 +1,6 @@
+var classlist = require('./classlist');
+var menuBtn = document.getElementsByClassName('menu-button')[0];
+menuBtn.addEventListener('click', function () {
+    var sidebar = document.getElementsByClassName('sidebar')[0];
+    classlist.toggleClass(sidebar, '-active');
+});
