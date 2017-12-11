25d6711a275cf0b1634b11c6098a9c19a34362d1
LAvAX <yuanxin@nbeport.com>
2017-12-11 17:20:10 +0800

    增加maptalks

---
+++ jsDemo/maptalks/src/animation/geometry-anim/index.js
@@ -0,0 +1,45 @@
+var map = new maptalks.Map('map', {
+    center: [-0.113049, 51.498568],
+    zoom: 14,
+    attribution: {
+        content: '$(attribution)'
+    },
+    baseLayer: new maptalks.TileLayer('base', {
+        urlTemplate: '$(urlTemplate)',
+        subdomains: $(subdomains)
+    })
+});
+var layer = new maptalks.VectorLayer('vector', {forceRenderOnMoving: true}).addTo(map);
+var marker = new maptalks.Marker(map.getCenter()).addTo(layer);
+
+var start = map.getCenter(),
+    offset = getOffset(),
+    end = start.add(offset);
+
+var arrow = new maptalks.LineString(
+    [start, end],
+    {
+        'id': 'arrow',
+        'arrowStyle': 'classic',
+        'arrowPlacement': 'vertex-last'
+    }
+).addTo(layer);
+
+replay();
+
+function replay() {
+    marker.setCoordinates(start);
+    marker.bringToFront().animate({
+        translate: [offset['x'], offset['y']]
+    }, {
+        duration: 2000,
+        focus: true
+    });
+}
+
+function getOffset() {
+    var center = map.getCenter();
+    var extent = map.getExtent();
+    marker.setCoordinates(center);
+    return extent.getMax().sub(map.getCenter()).multi(1 / 2);
+}
